3ca98b0b45740cf0610632c55a05065671bfb240
unknown <BookHaven>
2024-10-10 14:18:41 +0300

    Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    return n < 1 ? 0 : n<=2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2)
 };
+console.log(fibonacci(4));
+
 
 // Do not edit below this line
 module.exports = fibonacci;
